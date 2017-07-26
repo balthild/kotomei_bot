972f28ce43ec43250674d104679dd193bd23ad0b
Balthild Ires <ibalthild@gmail.com>
2017-07-26 19:09:55 +0800

    Pr other people

--- main.py
+++ main.py
@@ -13,11 +13,23 @@ class FilterPr(BaseFilter):
         return bool(message.text and self.regex.search(message.text) is not None)
 
 
+class FilterPrOther(BaseFilter):
+    def __init__(self):
+        self.regex = re.compile(r'/(pr)+(@.+)')
+
+    def filter(self, message):
+        return bool(message.text and self.regex.search(message.text) is not None)
+
+
 def pr(bot, update):
     if random.randint(1, 100) <= config.react_rate:
         update.message.reply_text(random.choice(config.react_emoticons))
     else:
         update.message.reply_text(config.default_emoticon)
+
+
+def pr_other(bot, update):
+    update.message.reply_text(config.other_emoticon)
 
 
 def debug(bot, update):
@@ -30,6 +42,7 @@ def main():
     updater = Updater(config.token)
 
     updater.dispatcher.add_handler(MessageHandler(FilterPr(), pr))
+    updater.dispatcher.add_handler(MessageHandler(FilterPrOther(), pr_other))
     # updater.dispatcher.add_handler(MessageHandler(Filters.all, debug))
 
     updater.start_polling()
