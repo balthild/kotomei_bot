9c2bae469003d19ce5944786403b51bafed4ca83
Balthild Ires <ibalthild@gmail.com>
2017-07-26 19:11:03 +0800

    Configuration of pr other people

--- config.example.py
+++ config.example.py
@@ -2,3 +2,4 @@ token = '00000000:XXXXXXXXXX_XXXXXXXXXXXXXXXXXXXXXXXX'
 default_emoticon = '( >  < )'
 react_emoticons = ['⁄(⁄ ⁄•⁄ω⁄•⁄ ⁄)⁄', '(≧ ﹏ ≦)', '(*/ω＼*)', 'ヽ(*。>Д<)o゜', '(つ ﹏ ⊂)']
 react_rate = 5
+other_emoticon = '| ω・`)'
